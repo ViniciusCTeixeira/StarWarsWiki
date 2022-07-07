5b0014ff97daa6d69d79c932ad8c92b80cca71df
Vinicius Teixeira <viniciuscteixeira.rj@gmail.com>
2022-07-06 23:52:08 -0300

    fix

--- src/screens/starships/index.tsx
+++ src/screens/starships/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { ImageBackground, View, Text, FlatList, Linking, TouchableOpacity, Modal } from 'react-native';
-import { Botao } from '../../components/Botao';
-import { StarshipModal } from '../../components/modais/ModalNaves';
-import { getStarships} from '../../services/swapi';
-import { styles } from './styles';
+import React, {useEffect, useRef, useState} from 'react';
+import {ImageBackground, View, Text, FlatList, Linking, TouchableOpacity, Modal} from 'react-native';
+import {Botao} from '../../components/Botao';
+import {StarshipModal} from '../../components/modais/ModalNaves';
+import {getStarships} from '../../services/swapi';
+import {styles} from './styles';
 import BackgroundHome from "../../assets/images/backgroud_home.jpg"
 
 
@@ -66,64 +66,60 @@ export const Starship = () => {
             console.log(err)
         }).finally(() => setLoading(false))
     }, [page]);
-    
-    
+
+
     return (
         <ImageBackground source={BackgroundHome} resizeMode="cover" style={{flex: 1}}>
-        <View style={styles.container}>
-            <Text style={styles.title}>
-                Naves
-            </Text>
-
-
-            {loading ?
-                <Text style={styles.cardTitle}>
-                    Carregando...
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    Naves
                 </Text>
-                :
-                <>
-                    <FlatList
-                        data={starShipList}
-                        showsVerticalScrollIndicator={false}
+                {loading ?
+                    <Text style={styles.cardTitle}>
+                        Carregando...
+                    </Text>
+                    :
+                    <>
+                        <FlatList
+                            data={starShipList}
+                            showsVerticalScrollIndicator={false}
 
-                        renderItem={({ item, index }) => (
+                            renderItem={({item, index}) => (
 
-                            <>
-                                <View style={styles.textos}>
-                                    <TouchableOpacity
-                                        onPress={() => {
-                                            setModal(true)
-                                            setStarshipItem(item)
-                                        }}
-                                    >
-                                        <Text style={styles.cardTitle}>Nave: {item.name}</Text>
-                                        <Text style={styles.text}>Modelo: {item.model}</Text>
+                                <>
+                                    <View style={styles.textos}>
+                                        <TouchableOpacity
+                                            onPress={() => {
+                                                setModal(true)
+                                                setStarshipItem(item)
+                                            }}
+                                        >
+                                            <Text style={styles.cardTitle}>Nave: {item.name}</Text>
+                                            <Text style={styles.text}>Modelo: {item.model}</Text>
 
-                                    </TouchableOpacity>
+                                        </TouchableOpacity>
+                                    </View>
+                                </>
+                            )
+                            }
+                        />
+                        <StarshipModal item={starshipItem} modal={modal} setModal={setModal} loading={false}
+                                       reload={false} setReload={setLoading}/>
+                    </>
+                }
 
+                <View style={styles.buttons}>
+                    <Botao titulo={'1'} corFundo={'transparent'} corTexto={'white'}
+                           onPress={() => setPage("?page=1")}></Botao>
+                    <Botao titulo={'2'} corFundo={'transparent'} corTexto={'white'}
+                           onPress={() => setPage('?page=2')}></Botao>
+                    <Botao titulo={'3'} corFundo={'transparent'} corTexto={'white'}
+                           onPress={() => setPage("?page=3")}></Botao>
+                    <Botao titulo={'4'} corFundo={'transparent'} corTexto={'white'}
+                           onPress={() => setPage('?page=4')}></Botao>
+                </View>
 
-                                </View>
-
-
-
-                            </>
-                        )
-
-                        }
-                    />
-                    <StarshipModal item={starshipItem} modal={modal} setModal={setModal} loading={false} reload={false} setReload={setLoading} />
-
-                </>
-            }
-
-            <View style={styles.buttons}>
-                <Botao titulo={'1'} corFundo={'transparent'} corTexto={'white'} onPress={() => setPage("?page=1")}></Botao>
-                <Botao titulo={'2'} corFundo={'transparent'} corTexto={'white'} onPress={() => setPage('?page=2')}></Botao>
-                <Botao titulo={'3'} corFundo={'transparent'} corTexto={'white'} onPress={() => setPage("?page=3")}></Botao>
-                <Botao titulo={'4'} corFundo={'transparent'} corTexto={'white'} onPress={() => setPage('?page=4')}></Botao>
             </View>
-
-        </View>
         </ImageBackground>
     )
-        }
+}

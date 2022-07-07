a07a1616432630d7c1b356304fa1ff79073917ba
Cauã Stulpen Mello <102328667+cauasmello@users.noreply.github.com>
2022-07-07 16:32:14 -0300

    starships atualizada

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {ThemeProvider} from '@rneui/themed';
 import AnimatedSplash from "react-native-animated-splash-screen";
 
 import {Stack} from "./Routes";
-
+import { Starship } from './screens/starships';
 import Logo from './assets/images/logo.png'
 import {ProvedorPost} from "./context/community";
 
@@ -29,7 +29,7 @@ export default function App() {
                 logoWidth={150}
             >
                 <ProvedorPost>
-                    <Stack/>
+                    <Starship/>
                 </ProvedorPost>
             </AnimatedSplash>
         </ThemeProvider>
